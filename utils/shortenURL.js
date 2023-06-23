79e82e4e08071419a11a10b6be7f3e289a218197
freeanser <bnn01615@gmail.com>
2023-06-23 16:00:25 +0800

    feat: add shortenURL function

--- utils/shortenURL.js
+++ utils/shortenURL.js
@@ -1,6 +1,25 @@
-let collection = []
+// 產出隨機短網址
+// 短網址格式是否為 5 組英數亂碼
+
 const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
 const UpperCaseLetters = lowerCaseLetters.toUpperCase()
 const numbers = '1234567890'
+// 隨機數需要的材料
+let collection = []
+collection = lowerCaseLetters + UpperCaseLetters + numbers
+const collectionLength = collection.length
+const max = collectionLength - 1 // 61
+const min = 0
+let shortURL = ''
 
-collection = collection.concat
+function randomNumber(count) {
+  for (i = 0; i < count; i++) {
+    // 產生亂數 Index // Math.floor(Math.random() * 10) // 0 <= n < 10
+    let randomIndex = Math.floor(Math.random() * (max - min + 1)) + min // 0 <= n <= 61
+    shortURL += collection[randomIndex]
+    // return shortURL
+  }
+}
+
+randomNumber(5)
+console.log('global shortURL:', shortURL)
