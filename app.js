6d7bb1d7962260035fd6d181e9f607297b620521
freeanser <bnn01615@gmail.com>
2023-06-24 15:31:07 +0800

    fix app.post function

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(methodOverride('_method')) // 結果沒有用到
 function generateRandomURL(length) {
   const shortURL = shortenURL(length);
   // 在此處使用短網址...
-  console.log(shortURL);
+  // console.log(shortURL);
   return shortURL
 }
 generateRandomURL(5)
@@ -35,51 +35,59 @@ app.get("/", (req, res) => {
 })
 
 app.post("/", (req, res) => {
+
   const reqURL = req.body.url
   const shortURL = generateRandomURL(5)
+  console.log('shortURL:', shortURL)
+  if (!reqURL) {
+    return res.redirect('/')
+  }
 
-  res.render("index", { reqURL })
-  // 從資料庫中找出 reqURL
-  // 如果找到了，就渲染出來 // 如果沒有，就建立新的資料，然後渲染出來 
   //三元運算符 // data ? a : b // 如果 data 有值，它會執行 a，否則它會執行 b
-  URL.findOne({ originalURL: reqURL }) // originalURL中有沒有 reqURL
-    // 當這個查詢成功完成時，它將返回找到的 URL 文件，並且這個結果將作為 data 傳遞到下一個 .then() 塊
+  URL.findOne({ originalURL: reqURL }) // 從資料庫中找出originalURL中有沒有 reqURL
+    // 當這個查詢成功完成時，它將返回找到的 URL 文件，並且這個結果將作為 data 傳遞到下一個 .then() 
+
     .then(data => {
+      // 如果沒有，就建立新的資料，然後渲染出來
       if (!data) {
         return URL.create({
-          shortURL: shortenURL,
+          shortURL,
           originalURL: reqURL
         })
       }
       return data
     })
+
+    // 如果找到了，就渲染出來
     .then(data => {
       res.render('index', {
-        orginal: req.headers.orginal,
-        shortURL: data.shortURL
+        origin: req.headers.origin,
+        shortURL: data.shortURL,
+        reqURL
       })
     })
     .catch(error => console.error(error))
+})
 
-  app.get('/:shortURL', (req, res => {
-    const { shortURL } = req.params
-    // 先去資料庫中找到 shortURL
-    URL.findOne({ shortURL })
-      .then(data => {
-        // 找不到資料 ＝> error 頁面
-        if (!data) {
-          return render('error', {
-            errorMsg: 'Can not find the Link.',
-            errorURL: req.headers.host + '/' + shortURL,
-          })
-        }
+app.get('/:shortURL', (req, res) => {
+  const { shortURL } = req.params
+  // 先去資料庫中找到 shortURL
+  URL.findOne({ shortURL })
+    .then(data => {
+      // 找不到資料 ＝> error 頁面
+      if (!data) {
+        return res.render('error', {
+          errorMsg: 'Can not find the Link.',
+          errorURL: req.headers.host + '/' + shortURL,
+        })
+      } if (data) {
         // 找到資料
         res.redirect(data.originalURL)
-      })
-      .catch(error => console.error(error))
-  }))
-  // console.log(shortenURL)
+      }
+    })
+    .catch(error => console.error(error))
 })
+
 
 
 app.listen(port, () => {
